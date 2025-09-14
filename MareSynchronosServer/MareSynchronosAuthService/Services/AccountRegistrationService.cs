3aa4e62f28861e3b7988b662b859ca69d53a66af
Eauldane <elf@eauldane.com>
2025-09-14 23:09:57 +0100

    Yeah this should just be a string.StartsWith.

--- MareSynchronosServer/MareSynchronosAuthService/Services/AccountRegistrationService.cs
+++ MareSynchronosServer/MareSynchronosAuthService/Services/AccountRegistrationService.cs
@@ -32,7 +32,7 @@ public class AccountRegistrationService
     private readonly ILogger<AccountRegistrationService> _logger;
     private readonly ConcurrentDictionary<string, IpRegistrationCount> _registrationsPerIp = new(StringComparer.Ordinal);
 
-    private Regex _registrationUserAgentRegex = new Regex(@"^MareSynchronos/", RegexOptions.Compiled, TimeSpan.FromMilliseconds(50));
+    
 
     public AccountRegistrationService(MareMetrics metrics, MareDbContext mareDbContext,
 		IServiceScopeFactory serviceScopeFactory, IConfigurationService<AuthServiceConfiguration> configuration,
@@ -49,7 +49,7 @@ public class AccountRegistrationService
     {
 		var reply = new RegisterReplyV2Dto();
 
-		if (!_registrationUserAgentRegex.Match(ua).Success)
+		if (!ua.StartsWith("MareSynchronos/", StringComparison.Ordinal))
         {
             reply.ErrorMessage = "User-Agent not allowed";
             return reply;
